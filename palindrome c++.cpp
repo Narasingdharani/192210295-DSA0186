cee39c349dde427fcfe88a76de2a180449d90c0f
Narasingdharani <130465865+Narasingdharani@users.noreply.github.com>
2024-08-22 18:28:03 +0530

    Add files via upload

---
+++ palindrome c++.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,n,rem,rev=0;
+  cout<<"ENTER THE NUMBER :";
+  cin>>a;
+  n=a;
+  while(n!=0)
+  {
+    rem=n%10;
+    rev=rev*10+rem;
+    n=n/10;
+  }
+  if(a==rev)
+  {
+    cout<<"\nPALLINDROME";
+  }
+  else
+  {
+    cout<<"\nNOT A PALINDROME";
+  }
+}
